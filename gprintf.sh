41d913e9eeef544fcd2d1046def08668cc88933a
feliciahsieh <felicia@tixwriteoff.com>
2017-06-19 05:01:27 +0000

    bash shell scripts to compile quicker

---
+++ gprintf.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+gcc -Wall -pedantic -Werror -Wextra main.c _printf.c _putchar.c
+./a.out
---
+++ run3.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+gcc -Wall -pedantic -Werror -Wextra 3-main.c 3-print_all.c -o d
+./d
