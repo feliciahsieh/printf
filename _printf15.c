3d4119dcaa42fde974f9fdaa1ec70fa4ba0c6a6e
feliciahsieh <felicia@tixwriteoff.com>
2017-06-20 16:45:08 +0000

    cleaning up and reverting back to 1 version back to printf1.c

---
+++ _printf15.c
@@ -0,0 +1,41 @@
+#include <stdarg.h>
+#include <stddef.h>
+#include "holberton.h"
+#define R13 55
+/**
+ * printf_rot13 - prints rot13 of the data
+ * @v: variatic list
+ * Return: total characters printed
+ */
+int printf_rot13(va_list v)
+{
+	char *str;
+	unsigned int total = 0, i = 0, j = 0;
+	char in[R13] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char out[R13] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	str = va_arg(v, char *);
+	while (str && str[i])
+	{
+		while (j < (R13 + 1))
+		{
+			if (str[i] == in[j])
+			{
+				_putchar(out[j]);
+				total++;
+				break;
+			}
+			else if (str[i] == '\n')
+			{
+				_putchar('\n');
+				total++;
+				break;
+			}
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+
+	return (total);
+}
---
+++ _printf4.c
@@ -0,0 +1,14 @@
+#include <stdarg.h>
+#include <stddef.h>
+#include "holberton.h"
+
+/**
+ * printf_octal - prints octal numbers
+ * @v: variatic list
+ * Return: total number of characters printed
+ */
+int printf_octal(va_list v)
+{
+	(void) v;
+	return(0);
+}
