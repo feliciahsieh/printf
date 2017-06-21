6d5137a1b5d05e181ff8b2341304edfbecc022ee
feliciahsieh <felicia@tixwriteoff.com>
2017-06-21 04:51:27 +0000

    add printf octal functionality

--- _printf4.c
+++ _printf4.c
@@ -1,14 +1,37 @@
 #include <stdarg.h>
-#include <stddef.h>
 #include "holberton.h"
-
 /**
- * printf_octal - prints octal numbers
+ * printf_octal - prints octal from printf
  * @v: variatic list
- * Return: total number of characters printed
+ * Return: number of characters printed
  */
 int printf_octal(va_list v)
 {
-	(void) v;
-	return (0);
+        int t, d, firstdigit = 0, total = 0;
+        unsigned int divisor, num = 0;
+        char octal[9] = "01234567";
+
+        num = va_arg(v, unsigned int);
+
+        divisor = 1073741824;
+        for (t = 0; t < 11; t++)
+        {
+                d = (num / divisor) % 8;
+                if ((d != 0) && !firstdigit)
+                        firstdigit = 1;
+                if (firstdigit)
+                {
+                        _putchar(octal[d]);
+                        total++;
+                }
+                divisor = divisor / 8;
+
+        }
+        if (total == 0)
+        {
+                _putchar(octal[d]);
+                total++;
+        }
+
+        return (total);
 }
